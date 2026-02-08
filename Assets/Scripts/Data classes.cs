2dc9b8556ebf99271d1a427850338fd57f640630
ano1her-project <ano1her.project@gmail.com>
2026-02-08 16:55:33 +0100

    EnemyWaveData tweaked
    EnemyPlacement removed, created FromEnemyAmongFiller() and FromEnemiesAmongFiller() methods

--- Assets/Scripts/Data classes.cs
+++ Assets/Scripts/Data classes.cs
@@ -43,7 +43,7 @@ public class EnemyData
         gun = p_gun;
     }
 
-    public void Spawn(Vector3 position)
+    public GameObject Spawn(Vector3 position)
     {
         var inst = GameObject.Instantiate(prefab, position, Quaternion.identity);
         inst.GetComponent<Rigidbody2D>().linearVelocity = Vector2.down * velocity;
@@ -51,43 +51,66 @@ public class EnemyData
             Object.Destroy(inst.GetComponent<ShipGun>());
         else
             inst.GetComponent<ShipGun>().data = gun;
-    }
-}
-
-public class EnemyPlacement
-{
-    public EnemyData enemy;
-    public float xPos;
-
-    public EnemyPlacement(EnemyData p_enemy, float p_xPos)
-    {
-        enemy = p_enemy;
-        xPos = p_xPos;
-    }
-
-    public void Spawn(float yPos)
-    {
-        enemy.Spawn(Vector3.up * yPos + Vector3.right * xPos);
+        return inst;
     }
 }
 
 public class EnemyWaveData
 {
-    public EnemyPlacement[] enemyPlacements;
+    public EnemyData[] enemies;
+    public float[] xs;
 
-    public static EnemyWaveData FromOneEnemyRepeatedSpaced(EnemyData enemy, int count, float bounds)
+    public EnemyWaveData(EnemyData[] p_enemies, float[] p_xs)
     {
-        var xs = SpacingGenerator.GetSpacedXs(count, bounds, 1f);
-        EnemyWaveData wave = new();
-        for (int i = 0; i < count; i++)
-            wave.enemyPlacements[i] = new(enemy, xs[i]);
-        return wave;
+        enemies = p_enemies;
+        xs = p_xs;
     }
 
-    public void Spawn(float yPos)
+    public static EnemyWaveData FromEnemyAmongFiller(EnemyData enemy, EnemyData filler, float[] xs)
     {
-        foreach (var placement in enemyPlacements)
-            placement.Spawn(yPos);
+        EnemyData[] enemies = new EnemyData[xs.Length];
+        enemies[Random.Range(0, xs.Length)] = enemy; // the one non-filler enemy
+        for (int i = 0; i < xs.Length; i++)
+        {
+            if (enemies[i] is null)
+                enemies[i] = filler;
+        }
+        return new(enemies, xs);
+    }
+
+    public static EnemyWaveData FromEnemiesAmongFiller(EnemyData[] nonFillerEnemies, EnemyData filler, float[] xs)
+    {
+        EnemyData[] enemies = new EnemyData[xs.Length];
+        List<int> vacantIndexes = new();
+        for (int i = 0; i < xs.Length; i++) // fill up vacantIndexes with numbers from 0 to n
+            vacantIndexes.Add(i);
+        // place non filler enemies
+        for (int e = 0; e < nonFillerEnemies.Length; e++)
+        {
+            int i = Random.Range(0, vacantIndexes.Count);
+            enemies[i] = nonFillerEnemies[e];
+            vacantIndexes.RemoveAt(i);
+        }
+        // fill all other spots with filler
+        for (int i = 0; i < xs.Length; i++) // using whatever's still left in vacantIndexes here is probably possible but feels more expensive, idk
+        {
+            if (enemies[i] is null)
+                enemies[i] = filler;
+        }
+        return new(enemies, xs);
+    }
+
+    public GameObject[] Spawn(float yPos)
+    {        
+        if (enemies.Length != xs.Length)
+        {
+            Debug.LogError("Enemy amount doesn't match amount of provided x positions! Spawn() aborted. Fuck you.");
+            return null;
+        }
+        var gameObjects = new GameObject[enemies.Length];
+        for (int i = 0; i < enemies.Length; i++)
+            gameObjects[i] = enemies[i].Spawn(Vector3.up * yPos + Vector3.right * xs[i]);
+        return gameObjects;
     }
 }
 
@@ -113,12 +136,22 @@ public class SpacingGenerator
     {
         float[] xs = new float[count];
         float x = -walls - objWidth / 2f;
-        float xIncrement = 2 * walls / (count + 1) - count / (count + 1) * objWidth + objWidth;
+        float xIncrement = 2 * walls / (count + 1) - count / (count + 1) * objWidth + objWidth; // just trust me bro
         for (int i = 0; i < count; i++)
         {
             x += xIncrement;
             xs[i] = x;
         }
         return xs;
+    }
+}
+
+public static class Extensions
+{
+    public static T[] Repeat<T>(this T obj, int count)
+    {
+        T[] result = new T[count];
+        System.Array.Fill(result, obj);
+        return result;
     }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float yPos;
+    public float xBounds;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
