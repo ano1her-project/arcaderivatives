83d5125b85b5f7a14ca0e8b19683afdaa8edd398
ano1her-project <ano1her.project@gmail.com>
2026-04-22 17:07:06 +0200

    Spacing.FromSetIncrement

--- Assets/Scripts/static util/Spacing.cs
+++ Assets/Scripts/static util/Spacing.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 public static class Spacing
 {
     public static float[] BetweenWalls(int count, float walls, float objWidth)
+    // the distance between the wall and the first ship is the same as the distance between ships
     // accounts for the width of the individual ships
     {
         float[] xs = new float[count];
         float x = -walls - objWidth / 2f;
-        float xIncrement = (2 * walls + objWidth) / (count + 1); // just trust me bro
+        float increment = (2 * walls + objWidth) / (count + 1); // just trust me bro
         for (int i = 0; i < count; i++)
         {
-            x += xIncrement;
+            x += increment;
             xs[i] = x;
         }
         return xs;
     }
+
+    public static float[] FromSetIncrement(int count, float increment)
+    // centered
+    // the distance between ships is set
+    // the distance between the wall and the first ship is not even considered
+    {
+        float[] xs = new float[count];
+        float x = -((count + 1) * increment) / 2f;
+        for (int i = 0; i < count; i++)
+        {
+            xs[i] = x;
+            x += increment;
+        }
+        return xs;
+    }
+
+    public static float[] FromSetGaps(int count, float objWidth, float gapWidth)
+        => FromSetIncrement(count, objWidth + gapWidth);
 }
