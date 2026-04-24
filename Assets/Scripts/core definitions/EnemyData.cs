31aba9bb66d64dcca0005e528c7385c9869301c5
ano1her-project <ano1her.project@gmail.com>
2026-04-24 16:55:08 +0200

    aim and link flags in EnemyData

--- Assets/Scripts/core definitions/EnemyData.cs
+++ Assets/Scripts/core definitions/EnemyData.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EnemyData
@@ -7,23 +8,35 @@ public class EnemyData
     public Sprite sprite;
     public GunData? gun;
     public float velocity;
+    public bool aimsAtPlayer;
+    public int linkedDerivative; // -1 if none
 
-    public EnemyData(Sprite p_sprite, GunData? p_gun, float p_velocity)
+    public EnemyData(Sprite p_sprite, GunData? p_gun, float p_velocity, bool p_aimsAtPlayer, int p_linkedDerivative)
     {
         sprite = p_sprite;
         gun = p_gun;
         velocity = p_velocity;
+        aimsAtPlayer = p_aimsAtPlayer;
+        linkedDerivative = p_linkedDerivative;
     }
+
+    public EnemyData(Sprite sprite, GunData? gun, float velocity) : this(sprite, gun, velocity, false, -1) { }
+    public EnemyData(Sprite sprite, GunData? gun, float velocity, bool aimsAtPlayer) : this(sprite, gun, velocity, aimsAtPlayer, -1) { }
+    public EnemyData(Sprite sprite, GunData? gun, float velocity, int linkedDerivative) : this(sprite, gun, velocity, false, linkedDerivative) { }
 
     public GameObject Spawn(Vector3 position)
     {
         var inst = GameObject.Instantiate(prefab, position, Quaternion.identity);
         inst.GetComponent<SpriteRenderer>().sprite = sprite;
         inst.GetComponent<Rigidbody2D>().linearVelocity = Vector2.down * velocity;
-        if (gun is null)
-            Object.Destroy(inst.GetComponent<AutoGun>());
-        else
-            inst.GetComponent<AutoGun>().data = gun;
+        // individual components
+        if (gun is not null)
+            inst.AddComponent<AutoGun>().data = gun;
+        if (aimsAtPlayer)
+            inst.AddComponent<AimAtPlayer>();
+        if (linkedDerivative != -1)
+            inst.AddComponent<LinkWithSlider>().derivativeIndex = linkedDerivative;
+        //
         return inst;
     }
 }
